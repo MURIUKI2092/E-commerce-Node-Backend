16e61a05930c51c344f13cddd1562d02c93fea6a
MURIUKI2092 <muriukijames33@gmail.com>
2022-03-26 08:58:04 +0300

    added auth logic for the user

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,72 @@
 const router = require('express').Router();
+const User= require('../models/user')
+const CryptoJs = require('crypto-js')
+const jwt = require('jsonwebtoken')
+
+//REGISTER function
+router.post ('/register',async(req,res)=>{
+
+  if (!req.username || !req.email ||!req.password){
+    return res.status(500).json({
+      message:"make sure you fill all the fields required"
+    })
+  }
+  else if(req.password.length<6){
+    return res.status(400).json({
+      message:"password length is shorter than required"
+    })
+  }
+ 
+  const newUser = new User ({
+    username: req.body.username,
+    email: req.body.email,
+    password:CryptoJs.AES.encrypt(req.body.password
+      ,process.env.secret_pass).toString() ,
+   
+  });
+  try{
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser)
+    
+  }catch(err){
+    res.status(500).json({
+      message:"register error"
+    })
+  }
+
+
+});
+
+//LOGIN function
+
+router.post("/login",async(req,res)=>{
+  try{
+    const user = await User.findOne({username:req.body.username})
+    if (!user){
+      res.status(401).json("Wrong credentials")
+    }
+    const hashedPassword = CryptoJs.AES.decrypt(
+      user.password,
+       process.secret_pass);
+    const OriginalPassword = hashedPassword.toString(CryptoJs.enc.Utf8)
+    
+    if (OriginalPassword !== req.body.password){
+      res.status(401).json("Wrong credentials")
+
+    }
+    const accessToken =jwt.sign({
+      id:user._id,
+      isAdmin:user.isAdmin
+
+    },process.env.jwt_key)
+
+    const {password ,...others}= user._doc;
+    res.status(200).json({...others,accessToken} )
+  }catch(err){
+    res.status(500).json(err)
+  }
+
+})
 
 
 
--- server.js
+++ server.js
@@ -5,16 +5,19 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
 const userRoute = require('./routes/user')
+const authRoute =require('./routes/auth')
 
-mongoose.connect(
+mongoose.
+connect(
   process.env.MONGO_URL
   )
-  .then(()=>console.log("DB Connection succesiful!!"))
+  .then(()=>console.log("DB Connection successful!!"))
   .catch ((err)=>{
     console.log("err");
   })
 app.use(express.json())
 app.use("/api/users",userRoute);
+app.use('/api/auth',authRoute)
 
 app.listen(process.env.PORT ||{port},()=>{
   console.log('backend is running')
